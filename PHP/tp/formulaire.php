1920fdc9383042d047522f7b62353745389f4516
Gcrouzet <58287057+Gcrouzet@users.noreply.github.com>
2020-04-01 09:27:01 +0200

    tp

---
+++ PHP/tp/formulaire.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tp </title>
+</head>
+
+<body>
+    <form action="secret.php" method="POST">
+        <p><label>Mot de passe <input type="text" name="mot_de_passe"></label></p>
+        <p><input type="submit"></p>
+
+    </form>
+</body>
+
+</html>
